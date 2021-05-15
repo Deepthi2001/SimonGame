31b93eb3ad492cfae19b86849461f7dfe1f83d56
Saideepthi <49442816+Deepthi2001@users.noreply.github.com>
2021-05-15 12:36:51 +0530

    Update simon.js

--- simon.js
+++ simon.js
@@ -4,14 +4,29 @@ var userClickedPattern = [];
 
 var started = false;
 var level = 0;
+var maxlevel=0;
 
-$(document).keypress(function() {
-  if (!started) {
-    $("#level-title").text("Level " + level);
-    nextSequence();
-    started = true;
-  }
-});
+
+function buttonClick1() {
+    if (!started) {
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        started = true;
+    }
+}
+function buttonClick2() {
+    playSound("wrong");
+      $("body").addClass("game-over");
+      $("#level-title").text("Game Exited !!");
+
+      setTimeout(function () {
+        $("body").removeClass("game-over");
+      }, 200);
+
+    level = 0;
+    gamePattern = [];
+    started = false;
+}
 
 $(".btn").click(function() {
 
@@ -28,6 +43,7 @@ function checkAnswer(currentLevel) {
 
     if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
       if (userClickedPattern.length === gamePattern.length){
+        $("#level-title").text("Good Keep going !!");
         setTimeout(function () {
           nextSequence();
         }, 1000);
@@ -35,24 +51,40 @@ function checkAnswer(currentLevel) {
     } else {
       playSound("wrong");
       $("body").addClass("game-over");
-      $("#level-title").text("Game Over, Press Any Key to Restart");
+      if(level>=maxlevel){
+        $("#level-title").text("Game Over!! Score: "+ level + " 🥇🏆");
+        alert("Congratulations. You have crossed old high score !!");
+      }
+      else{
+        $("#level-title").text("Game Over!! Score: "+ level);
+      }
+      
 
       setTimeout(function () {
         $("body").removeClass("game-over");
       }, 200);
-
-      startOver();
+      level = 0;
+      gamePattern = [];
+      started = false;
     }
 }
 
 function nextSequence() {
   userClickedPattern = [];
   level++;
-  $("#level-title").text("Level " + level);
+  
+  if(level>maxlevel){
+    $("#level-title").text("Hurray crossed highscore, Level " + level);
+    maxlevel=level;
+  }
+  else{
+    $("#level-title").text("Level " + level);
+  }
+  $("#highscore").text("HighScore: " + maxlevel);
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
-
+  
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
   playSound(randomChosenColour);
 }
@@ -69,8 +101,3 @@ function playSound(name) {
   audio.play();
 }
 
-function startOver() {
-  level = 0;
-  gamePattern = [];
-  started = false;
-}
